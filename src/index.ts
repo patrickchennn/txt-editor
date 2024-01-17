1a851c29ccfaac37658b724610ace881856d6807
patrickchennn <patrick.chen31@gmail.com>
2024-01-17 14:00:18 +0700

    add tailwind

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import "./index.css"
+
+
+const root = document.getElementById('app') as HTMLDivElement
+
+root.innerHTML = `
+  <div class="text-3xl font-bold underline">
+    hello world
+  </div>
+`
